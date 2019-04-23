28c1f68c81f972d06bd5833750526a1ded4b7479
Yasmin Ahmed <yasminahmed@yasmins-mbp.home>
2019-04-23 19:47:31 +0100

    removed files

--- src/store.js
+++ src/store.js
@@ -25,31 +25,22 @@ const reducer = (state = initialState, action) => {
                 data: action.payload.data
             }
         default:
-            console.log("Unknown action....: " + action.type)
+            //console.log("Unknown action....: " + action.type)
     }
 }
 
-//why is this here?
+
 const store = createStore(reducer);
 
-//order
-//REducer, auth until , 
-//as soon as someone signs in
+
 firebase.auth().onAuthStateChanged(user => {
     if(user){
-        console.log("setting user")
-        // store.dispatch({
-        //     type:"USER", //does it need to be capital?
-        //     payload: { user } 
-        // })
-        //goes to reducer 
-        //console.log("here")
-        //console.log(JSON.stringify(firebase.firebase()))
+        //console.log("setting user")
         let db = firebase.firestore()
         firebase.firestore().collection("Users").doc(user.uid).get()
         .then(function(doc) {
             if(doc.exists){
-                console.log(`if doc.exist -> user type: ${doc.data().Type}`)
+                
                 //CHILD
                 if(doc.data().Type === "child"){
                     const type = "child"
@@ -78,7 +69,7 @@ firebase.auth().onAuthStateChanged(user => {
                                 })
                             })
                             let data = {goal,age,name,gameProgress}
-                            console.log("children doc")
+               
                             store.dispatch({
                                 type: "DATA",
                                 payload: { data }
@@ -123,25 +114,15 @@ firebase.auth().onAuthStateChanged(user => {
                                                 date: doc.data().date
                                                 //data: doc.data()
                                             })
-                                            //console.log("gameData = "+ JSON.stringify(gameData))
-                                            //console.log(doc.id, " => ", doc.data());
+                                           
                                         });
-                                        //if(key2 === arr2.length-1){
-                                            //let allGames = [games]
                                             products.push({ //after each children iterations done then push into product array which holds all info
                                                 childID: cID,
                                                 childName: childName,
                                                 Games: gameData
                                             })
-                                            //let data = {name,products}
-                                            //console.log("inside 1")
-                                            //console.log("products inside 1: "+JSON.stringify(products))
-                                        //}
+                                   
                                         if(key === arr.length-1)  {
-                                            //let allGames = [games]
-                                            //let data = {name,products}
-                                            //console.log("inside 2")
-                                            //console.log("Product value inside IF 2: "+JSON.stringify(products))
                                             let data = {name,children,childrenNames,products}
                                             store.dispatch({
                                                 type: "DATA",
@@ -157,44 +138,16 @@ firebase.auth().onAuthStateChanged(user => {
                         })
 
                     })
-                    /*db.collection("Parent").doc(user.uid)
-                    .onSnapshot(function(doc){
-                        const name = doc.data().name;
-                        const children = doc.data().children
-                        const childrenNames = []
-                        const ref = firebase.firestore().collection("Children")
-                        children.forEach((item, key, arr) => { //for each, loop needs to finish 
-                            //console.log(" s item: "+item)
-                            ref.doc(item).get()
-                            .then(doc => {
-                                if(doc.exists){ //not needed
-                                    //console.log("here in store ")
-                                    childrenNames.push(doc.data().name)
-                                    //console.log("CS: "+childrenNames)
-                                    if(key === arr.length-1){
-                                        let data = {name,children,childrenNames}
-                                        console.log("store js: parent doc")
-                                        store.dispatch({
-                                            type: "DATA",
-                                            payload: { data }
-                                        })
-                                    }   
-                                }
-                                //previously else here with dispatch
-                            })
-
-                        })
-
-                    })*/
+                
                 }
             }
             else{
-                console.log("Doc does not exist in user collection")
+                //console.log("Doc does not exist in user collection")
             }
         })        
     }
     else{
-        console.log("Store.js: No current user")
+        //console.log("Store.js: No current user")
     }
 })
 
